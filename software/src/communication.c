920f275c460195d972734b903600a270b5840ce6
Olaf Lüke <olaf@tinkerforge.com>
2020-03-27 16:40:08 +0100

    Add initial API

--- software/src/communication.c
+++ software/src/communication.c
@@ -1,5 +1,5 @@
 /* silent-stepper-v2-bricklet
- * Copyright (C) 2019 Olaf Lüke <olaf@tinkerforge.com>
+ * Copyright (C) 2020 Olaf Lüke <olaf@tinkerforge.com>
  *
  * communication.c: TFP protocol message handling
  *
@@ -23,18 +23,122 @@
 
 #include "bricklib2/utility/communication_callback.h"
 #include "bricklib2/protocols/tfp/tfp.h"
+#include "bricklib2/logging/logging.h"
+
+#include "tmc5160.h"
 
 BootloaderHandleMessageResponse handle_message(const void *message, void *response) {
 	switch(tfp_get_fid_from_message(message)) {
-
+		case FID_SET_MOTION_CONFIGURATION: return set_motion_configuration(message);
+		case FID_GET_MOTION_CONFIGURATION: return get_motion_configuration(message, response);
+		case FID_SET_CURRENT_POSITION: return set_current_position(message);
+		case FID_GET_CURRENT_POSITION: return get_current_position(message, response);
+		case FID_SET_TARGET_POSITION: return set_target_position(message);
+		case FID_GET_TARGET_POSITION: return get_target_position(message, response);
+		case FID_SET_STEPS: return set_steps(message);
+		case FID_GET_STEPS: return get_steps(message, response);
+		case FID_GET_REMAINING_STEPS: return get_remaining_steps(message, response);
+		case FID_SET_STEP_CONFIGURATION: return set_step_configuration(message);
+		case FID_SET_ENABLED: return set_enabled(message);
+		case FID_GET_ENABLED: return get_enabled(message, response);
+		case FID_WRITE_REGISTER: return write_register(message, response);
+		case FID_READ_REGISTER: return read_register(message, response);
 		default: return HANDLE_MESSAGE_RESPONSE_NOT_SUPPORTED;
 	}
 }
 
 
+BootloaderHandleMessageResponse set_motion_configuration(const SetMotionConfiguration *data) {
+
+	return HANDLE_MESSAGE_RESPONSE_EMPTY;
+}
+
+BootloaderHandleMessageResponse get_motion_configuration(const GetMotionConfiguration *data, GetMotionConfiguration_Response *response) {
+	response->header.length = sizeof(GetMotionConfiguration_Response);
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
+
+BootloaderHandleMessageResponse set_current_position(const SetCurrentPosition *data) {
+
+	return HANDLE_MESSAGE_RESPONSE_EMPTY;
+}
+
+BootloaderHandleMessageResponse get_current_position(const GetCurrentPosition *data, GetCurrentPosition_Response *response) {
+	response->header.length = sizeof(GetCurrentPosition_Response);
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
+
+BootloaderHandleMessageResponse set_target_position(const SetTargetPosition *data) {
+
+	return HANDLE_MESSAGE_RESPONSE_EMPTY;
+}
+
+BootloaderHandleMessageResponse get_target_position(const GetTargetPosition *data, GetTargetPosition_Response *response) {
+	response->header.length = sizeof(GetTargetPosition_Response);
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
+
+BootloaderHandleMessageResponse set_steps(const SetSteps *data) {
+
+	return HANDLE_MESSAGE_RESPONSE_EMPTY;
+}
+
+BootloaderHandleMessageResponse get_steps(const GetSteps *data, GetSteps_Response *response) {
+	response->header.length = sizeof(GetSteps_Response);
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
+
+BootloaderHandleMessageResponse get_remaining_steps(const GetRemainingSteps *data, GetRemainingSteps_Response *response) {
+	response->header.length = sizeof(GetRemainingSteps_Response);
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
+
+BootloaderHandleMessageResponse set_step_configuration(const SetStepConfiguration *data) {
+
+	return HANDLE_MESSAGE_RESPONSE_EMPTY;
+}
+
+BootloaderHandleMessageResponse set_enabled(const SetEnabled *data) {
+
+	return HANDLE_MESSAGE_RESPONSE_EMPTY;
+}
+
+BootloaderHandleMessageResponse get_enabled(const GetEnabled *data, GetEnabled_Response *response) {
+	response->header.length = sizeof(GetEnabled_Response);
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
 
 
+BootloaderHandleMessageResponse write_register(const WriteRegister *data, WriteRegister_Response *response) {
+	tmc5160_task_register_write(data->reg, data->value);
 
+	response->header.length = sizeof(WriteRegister_Response);
+	response->status        = tmc5160.last_status;
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
+
+BootloaderHandleMessageResponse read_register(const ReadRegister *data, ReadRegister_Response *response) {
+	if(data->reg > 127) {
+		return HANDLE_MESSAGE_RESPONSE_INVALID_PARAMETER;
+	}
+
+	logd("read_register: %d\n\r", data->reg);
+
+	response->header.length = sizeof(ReadRegister_Response);
+	response->value         = tmc5160_task_register_read(data->reg);
+	response->status        = tmc5160.last_status;
+
+	logd(" -> %b %x\n\r", response->status, response->value);
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
 
 
 void communication_tick(void) {
--- software/src/communication.h
+++ software/src/communication.h
@@ -1,5 +1,5 @@
 /* silent-stepper-v2-bricklet
- * Copyright (C) 2019 Olaf Lüke <olaf@tinkerforge.com>
+ * Copyright (C) 2020 Olaf Lüke <olaf@tinkerforge.com>
  *
  * communication.h: TFP protocol message handling
  *
@@ -35,6 +35,21 @@ void communication_init(void);
 
 // Constants
 
+#define SILENT_STEPPER_V2_STEP_RESOLUTION_1 8
+#define SILENT_STEPPER_V2_STEP_RESOLUTION_2 7
+#define SILENT_STEPPER_V2_STEP_RESOLUTION_4 6
+#define SILENT_STEPPER_V2_STEP_RESOLUTION_8 5
+#define SILENT_STEPPER_V2_STEP_RESOLUTION_16 4
+#define SILENT_STEPPER_V2_STEP_RESOLUTION_32 3
+#define SILENT_STEPPER_V2_STEP_RESOLUTION_64 2
+#define SILENT_STEPPER_V2_STEP_RESOLUTION_128 1
+#define SILENT_STEPPER_V2_STEP_RESOLUTION_256 0
+
+#define SILENT_STEPPER_V2_RAMPING_MODE_POSITIONING 0
+#define SILENT_STEPPER_V2_RAMPING_MODE_VELOCITY_NEGATIVE 1
+#define SILENT_STEPPER_V2_RAMPING_MODE_VELOCITY_POSITIVE 2
+#define SILENT_STEPPER_V2_RAMPING_MODE_HOLD 3
+
 #define SILENT_STEPPER_V2_BOOTLOADER_MODE_BOOTLOADER 0
 #define SILENT_STEPPER_V2_BOOTLOADER_MODE_FIRMWARE 1
 #define SILENT_STEPPER_V2_BOOTLOADER_MODE_BOOTLOADER_WAIT_FOR_REBOOT 2
@@ -54,12 +69,163 @@ void communication_init(void);
 #define SILENT_STEPPER_V2_STATUS_LED_CONFIG_SHOW_STATUS 3
 
 // Function and callback IDs and structs
+#define FID_SET_MOTION_CONFIGURATION 1
+#define FID_GET_MOTION_CONFIGURATION 2
+#define FID_SET_CURRENT_POSITION 3
+#define FID_GET_CURRENT_POSITION 4
+#define FID_SET_TARGET_POSITION 5
+#define FID_GET_TARGET_POSITION 6
+#define FID_SET_STEPS 7
+#define FID_GET_STEPS 8
+#define FID_GET_REMAINING_STEPS 9
+#define FID_SET_STEP_CONFIGURATION 10
+#define FID_SET_ENABLED 11
+#define FID_GET_ENABLED 12
+#define FID_WRITE_REGISTER 13
+#define FID_READ_REGISTER 14
 
 
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t ramping_mode;
+	uint16_t velocity_start;
+	uint16_t acceleration_1;
+	uint16_t velocity_1;
+	uint16_t acceleration_max;
+	uint16_t velocity_max;
+	uint16_t deceleration_max;
+	uint16_t deceleration_1;
+	uint16_t velocity_stop;
+	uint16_t ramp_zero_wait;
+} __attribute__((__packed__)) SetMotionConfiguration;
+
+typedef struct {
+	TFPMessageHeader header;
+} __attribute__((__packed__)) GetMotionConfiguration;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t ramping_mode;
+	uint16_t velocity_start;
+	uint16_t acceleration_1;
+	uint16_t velocity_1;
+	uint16_t acceleration_max;
+	uint16_t velocity_max;
+	uint16_t deceleration_max;
+	uint16_t deceleration_1;
+	uint16_t velocity_stop;
+	uint16_t ramp_zero_wait;
+} __attribute__((__packed__)) GetMotionConfiguration_Response;
+
+typedef struct {
+	TFPMessageHeader header;
+	int32_t position;
+} __attribute__((__packed__)) SetCurrentPosition;
+
+typedef struct {
+	TFPMessageHeader header;
+} __attribute__((__packed__)) GetCurrentPosition;
+
+typedef struct {
+	TFPMessageHeader header;
+	int32_t position;
+} __attribute__((__packed__)) GetCurrentPosition_Response;
+
+typedef struct {
+	TFPMessageHeader header;
+	int32_t position;
+} __attribute__((__packed__)) SetTargetPosition;
+
+typedef struct {
+	TFPMessageHeader header;
+} __attribute__((__packed__)) GetTargetPosition;
+
+typedef struct {
+	TFPMessageHeader header;
+	int32_t position;
+} __attribute__((__packed__)) GetTargetPosition_Response;
+
+typedef struct {
+	TFPMessageHeader header;
+	int32_t steps;
+} __attribute__((__packed__)) SetSteps;
+
+typedef struct {
+	TFPMessageHeader header;
+} __attribute__((__packed__)) GetSteps;
+
+typedef struct {
+	TFPMessageHeader header;
+	int32_t steps;
+} __attribute__((__packed__)) GetSteps_Response;
+
+typedef struct {
+	TFPMessageHeader header;
+} __attribute__((__packed__)) GetRemainingSteps;
+
+typedef struct {
+	TFPMessageHeader header;
+	int32_t steps;
+} __attribute__((__packed__)) GetRemainingSteps_Response;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t step_resolution;
+	bool interpolation;
+} __attribute__((__packed__)) SetStepConfiguration;
+
+typedef struct {
+	TFPMessageHeader header;
+	bool enabled;
+} __attribute__((__packed__)) SetEnabled;
+
+typedef struct {
+	TFPMessageHeader header;
+} __attribute__((__packed__)) GetEnabled;
+
+typedef struct {
+	TFPMessageHeader header;
+	bool enabled;
+} __attribute__((__packed__)) GetEnabled_Response;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t reg;
+	uint32_t value;
+} __attribute__((__packed__)) WriteRegister;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t status;
+} __attribute__((__packed__)) WriteRegister_Response;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t reg;
+} __attribute__((__packed__)) ReadRegister;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t status;
+	uint32_t value;
+} __attribute__((__packed__)) ReadRegister_Response;
 
 
 // Function prototypes
-
+BootloaderHandleMessageResponse set_motion_configuration(const SetMotionConfiguration *data);
+BootloaderHandleMessageResponse get_motion_configuration(const GetMotionConfiguration *data, GetMotionConfiguration_Response *response);
+BootloaderHandleMessageResponse set_current_position(const SetCurrentPosition *data);
+BootloaderHandleMessageResponse get_current_position(const GetCurrentPosition *data, GetCurrentPosition_Response *response);
+BootloaderHandleMessageResponse set_target_position(const SetTargetPosition *data);
+BootloaderHandleMessageResponse get_target_position(const GetTargetPosition *data, GetTargetPosition_Response *response);
+BootloaderHandleMessageResponse set_steps(const SetSteps *data);
+BootloaderHandleMessageResponse get_steps(const GetSteps *data, GetSteps_Response *response);
+BootloaderHandleMessageResponse get_remaining_steps(const GetRemainingSteps *data, GetRemainingSteps_Response *response);
+BootloaderHandleMessageResponse set_step_configuration(const SetStepConfiguration *data);
+BootloaderHandleMessageResponse set_enabled(const SetEnabled *data);
+BootloaderHandleMessageResponse get_enabled(const GetEnabled *data, GetEnabled_Response *response);
+BootloaderHandleMessageResponse write_register(const WriteRegister *data, WriteRegister_Response *response);
+BootloaderHandleMessageResponse read_register(const ReadRegister *data, ReadRegister_Response *response);
 
 // Callbacks
 
